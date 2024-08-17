e5b44ba269d1f3ce97270b33b01eeaea1324c400
RychardAlcantara <rychardgabriell32@gmail.com>
2024-08-17 20:12:26 -0300

    feat: Aula sobre sintaxe e tipo de dados

--- estudos_C#/Program.cs
+++ estudos_C#/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models.Pessoa;
+
+DateTime dataHoraAtual = DateTime.Now;
+string horaCompleta = DateTime.Now.ToString("HH:mm:ss");
+string dataAtual = DateTime.Now.ToString("dd/MM/yyyy");
+Console.WriteLine(dataHoraAtual);
+Console.WriteLine(horaCompleta);
+Console.WriteLine(dataAtual);
+
+// string ap = "olaaaaaa";
+
+// int quantity = 1;
+
+// double price = 1.8000;
+
+// decimal price1 = 10.0000M;
+
+// Console.WriteLine(price1);
+// Console.WriteLine(price);
+// Console.WriteLine(ap);
+// Console.WriteLine(quantity);
+
+// Pessoa p = new Pessoa
+// {
+//     Nome = "John Doe",
+//     Idade = 10
+// };
+// p.Apresentar();
